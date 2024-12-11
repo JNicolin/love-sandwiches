96885ac9816e3fc481860cd02bf43002bab09165
Johan Nicolin <johan.nicolin@gmail.com>
2024-12-11 12:58:46 +0000

    returning validated data, requesting new input if invalid data

--- run.py
+++ run.py
@@ -15,14 +15,19 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     """ Get Sales figures input from the user 
     """
-    print("Please enter sales figures from the last market.")
-    print("Data shoule be six figures, separated by commas.")
-    print("Example: 10, 20, 30, 40, 50, 60\n")
+    while True:
+        print("Please enter sales figures from the last market.")
+        print("Data shoule be six figures, separated by commas.")
+        print("Example: 10, 20, 30, 40, 50, 60\n")
 
-    data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: ")
     
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+        if validate_data(sales_data):
+            print(f"Data is valid!")
+            break
+
+return sales_data
 
 def validate_data(values):
     """
@@ -38,6 +43,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again\n")
+        return False
 
-get_sales_data()
+    return True
+
+data = get_sales_data()
 
